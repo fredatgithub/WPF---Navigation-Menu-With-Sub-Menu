29b818740fe713847b42cc197b647b95036bd083
fred <domaine75@gmail.com>
2023-03-24 09:15:07 +0100

    clean code

---
+++ src/Navigation Menu App/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace Navigation_Menu_App
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void MaxBtn_Click(object sender, RoutedEventArgs e)
+    {
+      if (WindowState == WindowState.Normal)
+      {
+        WindowState = WindowState.Maximized;
+      }
+      else
+      {
+        if (WindowState == WindowState.Maximized)
+        {
+          WindowState = WindowState.Normal;
+        }
+      }
+    }
+
+    private void CloseBtn_Click(object sender, RoutedEventArgs e)
+    {
+      Close();
+    }
+  }
+}
